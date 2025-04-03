808b36470a71d09b35756f48789e291d5bd207c5
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2025-04-03 20:04:24 +0600

    .

---
+++ String_methods/all.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+// at()
+
+string str ("Test string");
+    for (unsigned i = 0; i < str.length(); i++) {
+        cout << str.at(i);
+    }
+
+
+
+
+
+
+// back()
+
+string str ("hello world.");
+    str.back() = '!';
+    cout << str << sesh;
+
+
+
+// begin()
+
+string str = "Test string";
+    for (string::iterator it = str.begin(); it != str.end(); it++) {
+        cout << *it;
+    }
+
+
+
+
